507a2f2f7daebf404c2d706db6d9f6823c2d5490
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-03-05 11:31:15 +0100

    models, admin

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+#from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Task (models.Model):
+    content = models.CharField((u'task'),max_length= 225) 
+    is_resolved=models.BooleanField((u'Resolved?'))
+
+
+def __unicode__(self):
+    return u'Task %s' % (self.id, self.id, self.content)                           
+
